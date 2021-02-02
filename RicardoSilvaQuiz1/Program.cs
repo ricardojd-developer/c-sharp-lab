9a4322d0ccb7a1ca9dc0c487ebde805daa266684
ricardojd-developer <ricardojd@gmail.com>
2021-02-01 19:37:54 -0800

    quiz1 1275

---
+++ RicardoSilvaQuiz1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RicardoSilvaQuiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the total charges for the food purchases:");
+            Double vTotal = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Total Before Taxes and Tip:  {0:C}" , vTotal);
+            Console.WriteLine("After tax (7%): {0:C} " ,   CalculateBillWithTax(vTotal));
+            Console.WriteLine("Tip (15%):  {0:C} " , AfterTax(CalculateBillWithTax(vTotal)));
+            Console.WriteLine("Total including Taxes with 15% Tip: {0:C} " , CalculateTip(AfterTax(CalculateBillWithTax(vTotal)), CalculateBillWithTax(vTotal)));
+            Console.ReadKey();
+
+        }
+
+        public static double CalculateBillWithTax(double v1)
+        {
+           double vCalc = (v1 * 7) / 100;
+            
+            return vCalc + v1;
+        }
+
+        public static double AfterTax (double v2)
+        {
+            double vTax = (v2 * 15) /100;
+            return vTax;
+        }
+
+        public static double CalculateTip (double v1,  double v3)
+        {
+
+               return v1 + v3;
+        }
+
+
+    }
+}
